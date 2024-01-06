7992389c4acc0e2916fc82b7c7b72bbcd6d01e57
Swiftlyll <moraless.flo@gmail.com>
2024-01-05 22:31:05 -0800

    Added extra installations

--- Bash/fedora_config.sh
+++ Bash/fedora_config.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 
+#   Author         : Kennet Morales
+#   Date           : January 01, 2024    
+#   GitHub         : https://github.com/Swiftlyll
 
 
 ### DNF Config ###
 echo -e "\n# Additions #\nfastestmirror=True\nmax_parallel_downloads=10\ndefaultyes=True" \
     | sudo tee -a /etc/dnf/dnf.conf
 
-
+sudo /usr/bin/dnf update && sudo /usr/bin/dnf upgrade -y
 
 ### First-time Configuration ###
 
@@ -39,11 +42,13 @@ sudo systemctl enable libvirtd
 
 ### Applications and Themes ###
 
-sudo /usr/bin/dnf update && sudo /usr/bin/dnf upgrade -y
-
-sudo /usr/bin/dnf install vim git kitty vlc \
+sudo /usr/bin/dnf install vim git kitty vlc nmap wireshark \
     papirus-icon-theme sassc gtk-murrine-engine gnome-tweaks -y
 
+# Codium
+sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
+sudo dnf install codium
 
 mkdir -p $HOME/Themes
 mkdir -p $HOME/.themes
