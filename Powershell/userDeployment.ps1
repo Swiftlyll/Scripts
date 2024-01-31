e36f06f2b759d424ba4de18ec03760b5c446a006
Swiftlyll <moraless.flo@gmail.com>
2024-01-30 17:56:19 -0800

    added to user depo

--- Powershell/userDeployment.ps1
+++ Powershell/userDeployment.ps1
@@ -7,6 +7,7 @@ Install-PackageProvider -Name NuGet -Force
 Set-PSRepository WingetTools -InstallationPolicy Trusted 
 Install-Module -Name WingetTools -Confirm:$false
 #>
+""
 
 <### List of Applications ###>
 
@@ -22,12 +23,37 @@ if ($null -ne $googleTrue) {
 } else {
     Write-Host -ForegroundColor Red "$google is not installed."
     Write-Output "Attempting to install $google..."
+    winget.exe install --Id Google.Chrome --accept-source-agreements --accept-package-agreements --disable-interactivity --silent
 }
 
+""
+
 # Office 365 #
+$office = "Office 16 Click-to-Run Extensibility Component"
+$officeTrue = $installedApplications | Where-Object {$_.Name -eq $office}
+$officeVersion = $officeTrue | Select-Object -ExpandProperty Version
 
+if ($null -ne $officeTrue) {
+    Write-Host -ForegroundColor Yellow "Office 365 Installed."
+    Write-Host -ForegroundColor Yellow "Version Number: $officeVersion"
+    
+} else {
+    Write-Host -ForegroundColor Red "Office 365 is not installed."
+    Write-Output "Attempting to install Office 365..."
+}
 
-
+<### Configurations ###>
 
 $userDesktop = [Environment]::GetFolderPath("DesktopDirectory")
 
+# Office Shortcuts #
+# Copy-Item -Path [EXCEL APP] -Destination $userDesktop
+# Copy-Item -Path [OUTLOOK APP] -Destination $userDesktop
+# Copy-Item -Path [WORD APP] -Destination $userDesktop
+
+# OTHER #
+# Copy-Item -Path CONFIGFILE -Destination "C:\Windows"
+# Copy-Item -Path CONFIGFILE -Destination "C:\Program Files (x86)\TBA"
+
+
+""
