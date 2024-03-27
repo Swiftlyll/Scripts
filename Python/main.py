15ef290082a3d5195795b1178dcc510d798e0710
Swiftlyll <moraless.flo@gmail.com>
2024-03-26 18:38:08 -0700

    test

---
+++ Python/main.py
@@ -0,0 +1,25 @@
+# Dictionary
+associations = {
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    "five": 5
+}
+
+print ("number", associations["five"])
+
+# List
+numbers = [
+    "one",
+    "two",
+    "three",
+]
+
+print (numbers[2])
+
+# for loop
+for association in associations:
+    print ("Haha")
+
+test
